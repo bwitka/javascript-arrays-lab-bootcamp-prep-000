cd542b59f66582b255556da882612a982885581c
BEAU WITKA <bwitka@gmail.com>
2018-07-21 04:00:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function destructivelyRemoveFirstKitten() {
 
 function prependKitten(name) {
   var nameArray = [name];
-  let newArray = nameArray.concat
+  let newArray = nameArray.concat()
   return newArray;
 }
 
